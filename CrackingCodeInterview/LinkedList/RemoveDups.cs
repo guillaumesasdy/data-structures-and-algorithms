08f638ffe593fa3b20d8dfb13d8b92a92381c99d
Guillaume Sasdy <guillaume.sasdy@gmail.com>
2020-01-09 01:01:07 +0100

    2.1 Remove dups: tests first

--- CrackingCodeInterview/LinkedList/RemoveDups.cs
+++ CrackingCodeInterview/LinkedList/RemoveDups.cs
@@ -1,6 +1,7 @@
 using System;
 using MySolutions.Common;
 using MySolutions.CrackingCodeInterview.SinglyLinkedList;
+using List = MySolutions.CrackingCodeInterview.SinglyLinkedList.SinglyLinkedList;
 
 namespace MySolutions.CrackingCodeInterview.LinkedList
 {
@@ -13,10 +14,48 @@ namespace MySolutions.CrackingCodeInterview.LinkedList
 
         public void Run()
         {
+            bool isRemoveDupsWithHashtablePassing = Test(RemoveDupsWithHashtable);
+            Console.WriteLine($"{nameof(RemoveDupsWithHashtable)} tests returns: {isRemoveDupsWithHashtablePassing}");
+
+            bool isRemoveDupsWithRunnersPassing = Test(RemoveDupsWithRunners);
+            Console.WriteLine($"{nameof(RemoveDupsWithRunners)} tests returns: {isRemoveDupsWithRunnersPassing}");
+        }
+
+        bool Test(Action<Node<char>> tested)
+        {
+            var twoDuplicatesList = List.Build<char>(new [] { 'a', 'a' });
+            tested(twoDuplicatesList);
+            var twoDuplicatesListExpectedResult = List.Build<char>(new [] { 'a' });
+
+            var threeDuplicatesList = List.Build<char>(new [] { 'a', 'a', 'a' });
+            tested(threeDuplicatesList);
+            var threeDuplicatesListExpectedResult = List.Build<char>(new [] { 'a' });
+
+            var duplicatedAtEnd = List.Build<char>(new [] { 'a', 'b', 'b' });
+            tested(duplicatedAtEnd);
+            var duplicatedAtEndExpectedResult = List.Build<char>(new [] { 'a', 'b' });
+
+            var noDuplicate = List.Build<char>(new [] { 'a', 'b', 'c' });
+            tested(noDuplicate);
+            var noDuplicateExpectedResult = List.Build<char>(new [] { 'a', 'b', 'c' });
+
+            var noDuplicateInMiddle = List.Build<char>(new [] { 'a', 'b', 'a' });
+            tested(noDuplicateInMiddle);
+            var noDuplicateInMiddleExpectedResult = List.Build<char>(new [] { 'a', 'b' });
+
+            return List.Equals(twoDuplicatesList, twoDuplicatesListExpectedResult)
+                && List.Equals(threeDuplicatesList, threeDuplicatesListExpectedResult)
+                && List.Equals(duplicatedAtEnd, duplicatedAtEndExpectedResult)
+                && List.Equals(noDuplicate, noDuplicateExpectedResult)
+                && List.Equals(noDuplicateInMiddle, noDuplicateInMiddleExpectedResult);
+        }
+
+        void RemoveDupsWithHashtable(Node<char> head)
+        {
             throw new NotImplementedException();
         }
 
-        Node<int> RemoveDupsInPlace(Node<int> head)
+        void RemoveDupsWithRunners(Node<char> head)
         {
             throw new NotImplementedException();
         }
