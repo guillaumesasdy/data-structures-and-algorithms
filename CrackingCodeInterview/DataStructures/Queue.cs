22c19d19fca831a2b9d5c6ce9ab7513e90f05c7a
Guillaume Sasdy <guillaume.sasdy@gmail.com>
2020-03-02 21:41:15 +0100

    Implement methods for Queue

--- CrackingCodeInterview/DataStructures/Queue.cs
+++ CrackingCodeInterview/DataStructures/Queue.cs
@@ -12,22 +12,42 @@ namespace MySolutions.CrackingCodeInterview.Queue
 
         public void Add(T value)
         {
-            throw new NotImplementedException();
+            var added = new Node<T>();
+            added.Value = value;
+
+            if (IsEmpty())
+                first = last = added;
+            else {
+                last.Next = added;
+                last = added;
+            }
         }
 
         public T Remove()
         {
-            throw new NotImplementedException();
+            if (IsEmpty())
+                throw new InvalidOperationException("The queue is empty.");
+            
+            var value = first.Value;
+            if (first == last)
+                first = last = null;
+            else {
+                first = first.Next;
+            }
+            return value;
         }
 
         public T Peek()
         {
-            throw new NotImplementedException();
+            if (IsEmpty())
+                throw new InvalidOperationException("The queue is empty.");
+            
+            return first.Value;
         }
 
         public bool IsEmpty()
         {
-            throw new NotImplementedException();
+            return first == null;
         }
 
         public void Explain()
